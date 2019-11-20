a5b15d838d87fc4c1798214b8425f3deb406e289
NikolaKordic <39223710+NikolaKordic@users.noreply.github.com>
2019-11-20 13:58:48 -0500

    Update Report Screen
    Report screen updated with back button (back to game)  very simple thus far

--- MassDroids/core/src/com/mygdx/game/screen/ReportScreen.java
+++ MassDroids/core/src/com/mygdx/game/screen/ReportScreen.java
@@ -1,5 +1,13 @@
 package com.mygdx.game.screen;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.scenes.scene2d.InputEvent;
+import com.badlogic.gdx.scenes.scene2d.ui.Button;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Skin;
+import com.badlogic.gdx.scenes.scene2d.utils.ClickListener;
+import com.badlogic.gdx.utils.Align;
+import com.mygdx.game.MyGame;
 import com.mygdx.game.base.GameBeta;
 import com.mygdx.game.base.ScreenBeta;
 
@@ -10,10 +18,47 @@ public class ReportScreen extends ScreenBeta {
         super(game);
     }
 
+    Button backToGameButton;
+    Label ReportNo1Label;
+    Label infrastructureText;
+    int titleFontSize;
+    int fontSize;
 
     @Override
     public void initialize()
     {
+        Skin skin = new Skin(Gdx.files.internal("skins/quantum-horizon/skin/quantum-horizon-ui.json"));
+        titleFontSize = 5;
+        fontSize = 3;
+
+
+        Label reportText = new Label("Reports", skin);
+        reportText.setPosition( MyGame.SCREEN_WIDTH * 0.45f, MyGame.SCREEN_HEIGHT * 0.7f);
+        reportText.setAlignment( Align.center );
+        reportText.setFontScale(6, 6);
+
+        uiStage.addActor(reportText);
+
+        ReportNo1Label = new Label("Report #1", skin);
+        ReportNo1Label.setFontScale(titleFontSize);
+        ReportNo1Label.setPosition( MyGame.SCREEN_WIDTH * 0.20f, MyGame.SCREEN_HEIGHT * 0.7f - 150);
+        uiStage.addActor(ReportNo1Label);
+
+        infrastructureText = new Label("This is where all of the text relating to a report would go", skin);
+        infrastructureText.setFontScale(fontSize);
+        infrastructureText.setWrap(true);
+        infrastructureText.setPosition( MyGame.SCREEN_WIDTH * 0.20f, MyGame.SCREEN_HEIGHT * 0.7f - 300);
+        uiStage.addActor(infrastructureText);
+
+
+
+        backToGameButton = new Button(skin);
+        backToGameButton.addListener(new ClickListener(){ public void clicked(InputEvent event, float x, float y) {
+                                             mainGame.setScreen( new GameplayScreen(mainGame) ); }});
+
+        backToGameButton.setSize(100,100);
+        backToGameButton.setPosition( MyGame.SCREEN_WIDTH * 0.50f, MyGame.SCREEN_HEIGHT * 0.7f - 600);
+        uiStage.addActor(backToGameButton);
     }
 
     @Override
