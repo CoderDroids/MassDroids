ae505e427f380d98057d543b1f4ad79dc8ae528e
hard-coded <cozyncoze@gmail.com>
2019-11-21 18:56:26 -0500

    updated game play screen ui

--- MassDroids/core/src/com/mygdx/game/screen/GameplayScreen.java
+++ MassDroids/core/src/com/mygdx/game/screen/GameplayScreen.java
@@ -33,6 +33,12 @@ public class GameplayScreen extends ScreenBeta {
     final float padWidth = 150;
     final float padHeight = 150;
 
+    Label woodResourceValue;
+    Label ironResourceValue;
+    Label goldResourceValue;
+    Label foodResourceValue;
+
+
     public GameplayScreen(GameBeta game )
     {
         super(game);
@@ -55,7 +61,7 @@ public class GameplayScreen extends ScreenBeta {
             };
         }
         );
-        Label configureLabel = new Label("Configure A.I", skin);
+        Label configureLabel = new Label("Configure A.I.", skin);
         configureLabel.setAlignment( Align.center );
         configureLabel.setFontScale(3.0f);
 
@@ -76,6 +82,39 @@ public class GameplayScreen extends ScreenBeta {
         Label resourceLabel = new Label("Resources", skin);
         resourceLabel.setAlignment( Align.center );
         resourceLabel.setFontScale(3.0f);
+
+        Label woodResourceLabel = new Label("Wood", skin);
+        woodResourceLabel.setAlignment( Align.center );
+        woodResourceLabel.setFontScale(3.0f);
+
+        Label ironResourceLabel = new Label("Iron", skin);
+        ironResourceLabel.setAlignment( Align.center );
+        ironResourceLabel.setFontScale(3.0f);
+
+        Label goldResourceLabel = new Label("Gold", skin);
+        goldResourceLabel.setAlignment( Align.center );
+        goldResourceLabel.setFontScale(3.0f);
+
+        Label foodResourceLabel = new Label("Food", skin);
+        foodResourceLabel.setAlignment( Align.center );
+        foodResourceLabel.setFontScale(3.0f);
+
+        woodResourceValue = new Label("0", skin);
+        woodResourceValue.setAlignment( Align.center );
+        woodResourceValue.setFontScale(3.0f);
+
+        ironResourceValue = new Label("0", skin);
+        ironResourceValue.setAlignment( Align.center );
+        ironResourceValue.setFontScale(3.0f);
+
+        goldResourceValue = new Label("0", skin);
+        goldResourceValue.setAlignment( Align.center );
+        goldResourceValue.setFontScale(3.0f);
+
+        foodResourceValue = new Label("0", skin);
+        foodResourceValue.setAlignment( Align.center );
+        foodResourceValue.setFontScale(3.0f);
+
 
         Image playerInfo = new Image(new Texture("badlogic.jpg"));
 
@@ -114,33 +153,48 @@ public class GameplayScreen extends ScreenBeta {
         table.row().height(100);
         table.add(configureButton).width(200).height(200);
         table.add().width(100);
+        table.add().width(100);
         table.add(reportButton).width(200).height(200);
         table.row();
         table.add(configureLabel).height(100);
+        table.add().width(100);
         table.add().width(100);
         table.add(reportLabel).height(100);
         table.row().height(50);
 
         // resouces info
-        table.add(resourceLabel).colspan(3);
+        table.add(resourceLabel).colspan(4);
         table.row();
-        table.add();
+
+        // resouces label
+        table.add(woodResourceLabel);
+        table.add(ironResourceLabel);
+        table.add(goldResourceLabel);
+        table.add(foodResourceLabel);
+        table.row();
+        table.add(woodResourceValue);
+        table.add(ironResourceValue);
+        table.add(goldResourceValue);
+        table.add(foodResourceValue);
+        table.row();
 
         // main player information
-        table.add(playerInfo);
+        table.add(playerInfo).width(SCREEN_WIDTH*0.8f).colspan(4).height(SCREEN_HEIGHT * 0.5f);
         table.row();
 
         // bottom button panel
         table.row().height(100);
         table.add(nextButton).width(200).height(200);
         table.add().width(100);
+        table.add().width(100);
         table.add(backButton).width(200).height(200);
         table.row();
         table.add(nextLabel).height(100);
         table.add().width(100);
+        table.add().width(100);
         table.add(backLabel).height(100);
         table.row().height(50);
-
+        //table.debug();
 
         uiStage.addActor(table);
 
