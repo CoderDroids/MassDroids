a74436c0cdd43719be441ff2a72fe674e81b8d97
Ted-bissada <tbissada@gmail.com>
2019-12-11 16:36:50 -0500

    Fixed mutiple issues
    - scaling now works at all map sizes
    - tapping screen sets buildium and gold labes to values for tapped tile for testing purposes

--- MassDroids/core/src/com/mygdx/game/Map/Tile.java
+++ MassDroids/core/src/com/mygdx/game/Map/Tile.java
@@ -2,11 +2,11 @@ package com.mygdx.game.Map;
 
 public class Tile
 {
-    int tileType;
-    int playerThatOwns;
-    int gold;
-    int buildium;
-    int defenders;
-    int attackers;
-    int defensiveValue;
+    public int tileType;
+    public int playerThatOwns;
+    public int gold;
+    public int buildium;
+    public int defenders;
+    public int attackers;
+    public int defensiveValue;
 }
